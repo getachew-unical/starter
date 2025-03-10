02a5d984f1afee99338a1d3be19baa906249c3ab
getachew-unical <146995175+getachew-unical@users.noreply.github.com>
2025-03-09 20:49:48 -0400

    scrollbar add

--- src/app/(full-width-pages)/(auth)/signin/page.tsx
+++ src/app/(full-width-pages)/(auth)/signin/page.tsx
@@ -1,34 +1,50 @@
 import SignInForm from "@/components/auth/SignInForm";
+import GridShape from "@/components/common/GridShape";
 import { Metadata } from "next";
-// import GridShape from "@/components/common/GridShape";
-// import Image from "next/image";
-// import Link from "next/link";
+import Image from "next/image";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "Next.js SignIn Page | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Signin Page TailAdmin Dashboard Template",
+  title:
+    "Adey ERP System - Utility Management",
+  description: "The Adey ERP System includes a Utility Management module designed to streamline and optimize the administration of utility services, such as electricity, water, gas, and telecommunications.",
+ icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme:light)', 
+        url: '/images/logo/favicon.ico', 
+        href: '/images/logo/favicon.ico',
+      },
+       {
+        media: '(prefers-color-scheme:dark)', 
+         url: '/images/logo/favicon.ico', 
+        href: '/images/logo/favicon.ico',
+      }
+    ],
+  },
 };
 
 export default function SignIn() {
   return (
     <div className="relative flex w-full h-screen px-4 py-6 overflow-hidden bg-white z-1 dark:bg-gray-900 sm:p-0">
       <SignInForm />
-      {/* <div className="relative items-center justify-center flex-1 hidden p-8 z-1 bg-brand-950 dark:bg-white/5 lg:flex">
+      <div className="relative items-center justify-center flex-1 hidden p-8 z-1 bg-brand-950 dark:bg-white/5 lg:flex">
+        {/* <!-- ===== Common Grid Shape Start ===== --> */}
         <GridShape />
         <div className="flex flex-col items-center max-w-xs">
           <Link href="/" className="block mb-4">
             <Image
               width={231}
               height={48}
-              src="./images/logo/auth-logo.svg"
+              src="/images/logo/favicon.ico"
               alt="Logo"
             />
           </Link>
           <p className="text-center text-gray-400 dark:text-white/60">
-            Free and Open-Source Tailwind CSS Admin Dashboard Template
+          Adey ERP System - Utility Management
           </p>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
--- src/app/(full-width-pages)/(auth)/signup/page.tsx
+++ src/app/(full-width-pages)/(auth)/signup/page.tsx
@@ -1,37 +1,51 @@
 import SignUpForm from "@/components/auth/SignUpForm";
+import GridShape from "@/components/common/GridShape";
 import { Metadata } from "next";
-// import GridShape from "@/components/common/GridShape";
-// import Image from "next/image";
-// import Link from "next/link";
+import Image from "next/image";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "Next.js SignUp Page | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js SignUp Page TailAdmin Dashboard Template",
-  // other metadata
+  title:
+    "Adey ERP System - Utility Management",
+  description: "The Adey ERP System includes a Utility Management module designed to streamline and optimize the administration of utility services, such as electricity, water, gas, and telecommunications.",
+ icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme:light)', 
+        url: '/images/logo/favicon.ico', 
+        href: '/images/logo/favicon.ico',
+      },
+       {
+        media: '(prefers-color-scheme:dark)', 
+         url: '/images/logo/favicon.ico', 
+        href: '/images/logo/favicon.ico',
+      }
+    ],
+  },
 };
 
 export default function SignUp() {
   return (
-    <div>
+    <div className="relative flex w-full h-screen overflow-hidden bg-white z-1 dark:bg-gray-900">
       <SignUpForm />
-      {/* <div className="relative items-center justify-center flex-1 hidden p-8 z-1 bg-brand-950 dark:bg-white/5 lg:flex">
-        
+      <div className="relative items-center justify-center flex-1 hidden p-8 z-1 bg-brand-950 dark:bg-white/5 lg:flex">
+        {/* <!-- ===== Common Grid Shape Start ===== --> */}
         <GridShape />
-        
+        {/* <!-- ===== Common Grid Shape End ===== --> */}
         <div className="flex flex-col items-center max-w-xs">
           <Link href="" className="block mb-4">
             <Image
-              src="/images/logo/auth-logo.svg"
+              src="/images/logo/favicon.ico"
               width={231}
               height={48}
               alt="Logo"
             />
           </Link>
           <p className="text-center text-gray-400 dark:text-white/60">
-            Free and Open-Source Tailwind CSS Admin Dashboard Template
+            Adey ERP System- Utility Management
           </p>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
