df90e8ed3e62dab200d75c86afce0ec814f0b491
getachew-unical <146995175+getachew-unical@users.noreply.github.com>
2025-03-09 22:14:03 -0400

    sample dashboard added

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import { EcommerceMetrics } from "@/components/ecommerce/EcommerceMetrics";
 import React from "react";
+import Image from "next/image";
+import { EcommerceMetrics } from "@/components/ecommerce/EcommerceMetrics";
 import MonthlyTarget from "@/components/ecommerce/MonthlyTarget";
 import MonthlySalesChart from "@/components/ecommerce/MonthlySalesChart";
 import StatisticsChart from "@/components/ecommerce/StatisticsChart";
@@ -29,8 +30,25 @@ export const metadata: Metadata = {
 
 export default function Ecommerce() {
   return (
-    <div className="grid grid-cols-12 gap-4 md:gap-6">
-      <div className="col-span-12 space-y-6 xl:col-span-7">
+    // <div className="grid grid-cols-12 gap-4 md:gap-6">
+    <div className="items-center flex flex-col gap-4 md:gap-6">
+      <h1 className="text-red-600">Dashboard</h1>
+      <p className="text-red-600">The Dashboard will be displayed here</p>
+      <p className="text-red-600"> Like the images below</p>
+              <Image className="mx-auto"
+                width={1100}
+                height={900}
+                src="/images/dashboard.png"
+                alt="dashboard"
+              />
+
+              <Image className="mx-auto"
+                width={1100}
+                height={900}
+                src="/images/sample.png"
+                alt="dashboard"
+              />
+      {/* <div className="col-span-12 space-y-6 xl:col-span-7">
         <EcommerceMetrics />
 
         <MonthlySalesChart />
@@ -50,7 +68,7 @@ export default function Ecommerce() {
 
       <div className="col-span-12 xl:col-span-7">
         <RecentOrders />
-      </div>
+      </div> */}
     </div>
   );
 }
